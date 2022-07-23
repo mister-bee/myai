e7d53f6f4f9f2c88dbe3c0a62f49e128fc5df894
mister-bee <thebeech@yahoo.com>
2022-07-23 17:48:25 -0500

    misc

--- src/App.js
+++ src/App.js
@@ -5,19 +5,15 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>My Ai</h1>
       </header>
+      <body>
+        <h1>List of pictures</h1>
+        <br />
+        <br />
+        <br />
+        <br />
+      </body>
     </div>
   );
 }
